d27a5cff59f9e33f3153f784bc2542ef4d167c07
Thomas Svenson <thomas@tsvenson.com>
2023-10-15 19:14:42 +0200

    Adds a simple choice menu and dummy options for 0 and 1.

--- pdf_tools.py
+++ pdf_tools.py
@@ -1,9 +1,12 @@
 from os import scandir
 
 
+LIBRARY_PATH = 'L:\Books'
+
+
 def find_pdfs_containing_string_in_name(search_string: str) -> None:
-    number_of = 0
-    for file_name in scandir('L:\Books'):
+    number_of: int = 0
+    for file_name in scandir(LIBRARY_PATH):
         if file_name.name.endswith('.pdf'):
             if search_string in file_name.name:
                 print(file_name.name)
@@ -12,11 +15,24 @@ def find_pdfs_containing_string_in_name(search_string: str) -> None:
     return None
 
 def main():
-    search_files = input('Enter string to search in file name for: ').strip()
-    if len(search_files) > 2:
-        find_pdfs_containing_string_in_name(search_files)
-    else:
-        print('You need to type in at least 3 character!')
+    print('\nSelect what you want to do:')
+    print('1 - List number of books in each file format.')
+    print('2 - List books where the filename contains a specific string.')
+    print('0 - Quit')
+    choice: int = int(input('Make choice: ').strip())
+    match choice:
+        case 0:
+            print('By for now.')
+        case 1:
+            print('Comming soon.')
+        case 2:
+            search_files = input('Enter string to search in file name for: ').strip()
+            if len(search_files) > 2:
+                find_pdfs_containing_string_in_name(search_files)
+            else:
+                print('You need to type in at least 3 character!')
+        case _:
+            print('That was a wrong option!')
 
 if __name__ == '__main__':
     main()
